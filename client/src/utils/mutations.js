914d6e2702ce4d6b17c4b9126db9e8508162acdc
Stephanie <whatawhat@hotmail.com>
2021-08-17 12:05:23 -0400

    added username for login user

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,7 +1,7 @@
 import gql from "graphql-tag";
 
 export const LOGIN_USER = gql`
-  mutation login($email: String!, $password: String!) {
+  mutation login($username: String!, $email: String!, $password: String!) {
     login(email: $email, password: $password) {
       token
       user {
